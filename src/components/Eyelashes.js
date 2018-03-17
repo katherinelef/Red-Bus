ac9cae8b86c860f9eab0752a956a348031f66e2b
JessicaMP <jessica95.mp@gmail.com>
2018-03-17 12:44:02 -0500

    Agregando seccion-component

--- src/components/Eyelashes.js
+++ src/components/Eyelashes.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Eyelashes = () => (
     <nav>
-        <ul className="nav nav-tabs" id="myTab" role="tablist">
-            <li className="nav-item">
-                <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Banca por internet</a>
+        <ul className="nav nav-tabs border-warning eyelashes" id="myTab" role="tablist">
+            <li className="nav-item ml-4">
+                <a className="nav-link active" id="home-tab" data-toggle="tab" href="#" role="tab" aria-controls="home" aria-selected="true">Banca por internet</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Pagar en efectivo</a>
+                <a className="nav-link" id="profile-tab" data-toggle="tab" href="#" role="tab" aria-controls="profile" aria-selected="false">Pagar en efectivo</a>
             </li>
         </ul>
         <div className="tab-content" id="myTabContent">
